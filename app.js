8434d418a335886b3b664ca48bfbb225c707d5b2
Mord Maman <jaime.mord.maman@gmail.com>
2017-02-10 15:15:59 +0000

    creates get route to / and app.listen to port 3000

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ var mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.send("home page");
+})
+
+//server 
+app.listen(3000, function(){
+    console.log('Listening on port 3000!');
+})
