a2b7efe2ce9af4e735c403281d34bfebec3e6182
Mord Maman <jaime.mord.maman@gmail.com>
2017-02-15 15:42:59 +0000

    sets up schema

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    date: String,
+    content: String
+});
+
+ var Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", function(req, res){
     res.render("index");
