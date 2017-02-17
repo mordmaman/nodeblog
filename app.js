87cee077cbc92762c4353d10f6447f7636e57865
Mord Maman <jaime.mord.maman@gmail.com>
2017-02-17 11:17:11 +0000

    adds blogs/new route

--- app.js
+++ app.js
@@ -16,24 +16,28 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create(
-    {
-        title: "Welcome",
-        image: "http://http://www.desicomments.com/wp-content/uploads/Welcome-Simple-Greeting-Image-P8820dc07-600x497.jpg",
-        date: "15/02/2017",
-        content: "Welcome to my blog"
-    }, function (err, blog){
-        if (err){
-            console.log(err);
-        } else {
-            console.log("New Blog");
-            console.log(blog);
-        }
-    }
-)
+// Blog.create(
+//     {
+//         title: "Welcome",
+//         image: "http://http://www.desicomments.com/wp-content/uploads/Welcome-Simple-Greeting-Image-P8820dc07-600x497.jpg",
+//         date: "15/02/2017",
+//         content: "Welcome to my blog"
+//     }, function (err, blog){
+//         if (err){
+//             console.log(err);
+//         } else {
+//             console.log("New Blog");
+//             console.log(blog);
+//         }
+//     }
+// )
 
 app.get("/", function(req, res){
     res.render("index");
+})
+
+app.get("blogs/new", function(req, res){
+    res.render("new.ejs");
 })
 
 //server 
